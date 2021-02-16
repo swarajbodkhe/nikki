698bcd0929c94bea126e815bbde459c8d6820f52
swaraj <swaraj203@gmail.com>
2021-02-16 15:56:37 +0530

    added for loop , working query

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,7 @@
 import net.andreinc.mockneat.MockNeat;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -25,106 +26,117 @@ public class Demo {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
-
-        driver.get("http://www.voot.com");
-        WebDriverWait wt = new WebDriverWait(driver, 10);
-
-        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@src,'Avatar')]"))));
-        WebElement AccountIcon = driver.findElement(By.xpath("//img[contains(@src,'Avatar')]"));
-        AccountIcon.click();
-        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//div[contains(@class,'MuiList')])[1]//span"))));
-        WebElement LogIn = driver.findElement(By.xpath("(//div[contains(@class,'MuiList')])[1]//span"));
-        LogIn.click();
+        WebDriver driver=null;
 
 
-        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//div[@role='dialog']//button)[3]"))));
-        WebElement ContinueWithEmail = driver.findElement(By.xpath("(//div[@role='dialog']//button)[3]"));
-        ContinueWithEmail.click();
+        for (int j = 0; j < 100; j++) {
+            try{
+                 driver= new ChromeDriver();
+                driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+                driver.get("http://www.voot.com");
+                WebDriverWait wt = new WebDriverWait(driver, 10);
 
-        //from here
-        WebElement email=driver.findElement(By.id("email"));
-        email.sendKeys(randomEmail());
-        email.sendKeys(Keys.ENTER);
-
-        WebElement pwd=driver.findElement(By.id("password"));
-        pwd.sendKeys("password");
-        pwd.sendKeys(Keys.ENTER);
-
-
-        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("name"))));
-        WebElement Name=driver.findElement(By.id("name"));
-        WebElement dob=driver.findElement(By.id("dob"));
-        WebElement gender=driver.findElement(By.xpath("//span[text()='Male']"));
-        WebElement pref=driver.findElement(By.xpath("//button//label[text()='Select your preference']"));
+                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@src,'Avatar')]"))));
+                WebElement AccountIcon = driver.findElement(By.xpath("//img[contains(@src,'Avatar')]"));
+                AccountIcon.click();
+                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//div[contains(@class,'MuiList')])[1]//span"))));
+                WebElement LogIn = driver.findElement(By.xpath("(//div[contains(@class,'MuiList')])[1]//span"));
+                LogIn.click();
 
 
-        Name.sendKeys(randomName());
-        dob.sendKeys("05/12/1992");
-        gender.click();
-        pref.click();
-        WebElement save=driver.findElement(By.xpath("//button[text()='Save']"));
-        save.click();
-        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@src,'purple')]/.."))));
-        WebElement agreeTerms=driver.findElement(By.xpath("//img[contains(@src,'purple')]/.."));
-        agreeTerms.click();
-        WebElement submit=driver.findElement(By.xpath("//button[text()='Submit']"));
-        submit.click();
-        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@src,'closeButton')]/.."))));
-        WebElement closeup1=driver.findElement(By.xpath("//img[contains(@src,'closeButton')]"));
-        //Actions atc=new Actions(driver);
-        //atc.moveToElement(closeup1,5,5).click().build().perform();
-       // closeup1.click();
-        //closeup1.sendKeys(Keys.F5);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.get(driver.getCurrentUrl());
-        // Till here
+                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//div[@role='dialog']//button)[3]"))));
+                WebElement ContinueWithEmail = driver.findElement(By.xpath("(//div[@role='dialog']//button)[3]"));
+                ContinueWithEmail.click();
 
-        try {
-            driver.manage().window().maximize();
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < 3; i++) {
-            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,document.body.scrollHeight)");
-        }
+                //from here
+                WebElement email=driver.findElement(By.id("email"));
+                email.sendKeys(randomEmail());
+                email.sendKeys(Keys.ENTER);
 
-        WebElement voteNow=driver.findElement(By.xpath("(//button[contains(@class,'interactivityPlayButton')])[1]"));
-        String mainWindowHandle = driver.getWindowHandle();
-        voteNow.click();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Set<String> allWindowHandles = driver.getWindowHandles();
-        Iterator<String> iterator = allWindowHandles.iterator();
+                WebElement pwd=driver.findElement(By.id("password"));
+                pwd.sendKeys("password");
+                pwd.sendKeys(Keys.ENTER);
 
-        // Here we will check if child window has other child windows and will fetch the heading of the child window
-        while (iterator.hasNext()) {
-            String ChildWindow = iterator.next();
-            if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
-                driver.switchTo().window(ChildWindow);
-                WebElement nikki = driver.findElement(By.xpath("//img[@alt='Nikki Tamboli']"));
-                nikki.click();
-                WebElement submitBtn=driver.findElement(By.xpath("//button[text()='Submit']"));
-                submitBtn.click();
+
+                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("name"))));
+                WebElement Name=driver.findElement(By.id("name"));
+                WebElement dob=driver.findElement(By.id("dob"));
+                WebElement gender=driver.findElement(By.xpath("//span[text()='Male']"));
+                WebElement pref=driver.findElement(By.xpath("//button//label[text()='Select your preference']"));
+
+
+                Name.sendKeys(randomName());
+                dob.sendKeys("05/12/1992");
+                gender.click();
+                pref.click();
+                WebElement save=driver.findElement(By.xpath("//button[text()='Save']"));
+                save.click();
+                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@src,'purple')]/.."))));
+                WebElement agreeTerms=driver.findElement(By.xpath("//img[contains(@src,'purple')]/.."));
+                agreeTerms.click();
+                WebElement submit=driver.findElement(By.xpath("//button[text()='Submit']"));
+                submit.click();
+                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@src,'closeButton')]/.."))));
+                WebElement closeup1=driver.findElement(By.xpath("//img[contains(@src,'closeButton')]"));
+                //Actions atc=new Actions(driver);
+                //atc.moveToElement(closeup1,5,5).click().build().perform();
+                // closeup1.click();
+                //closeup1.sendKeys(Keys.F5);
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//p[contains(text(),'Thank You for Voting')]"))));
+                driver.get(driver.getCurrentUrl());
+                // Till here
 
+                try {
+                    driver.manage().window().maximize();
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0; i < 3; i++) {
+                    ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,document.body.scrollHeight)");
+                }
+
+                WebElement voteNow=driver.findElement(By.xpath("(//button[contains(@class,'interactivityPlayButton')])[1]"));
+                String mainWindowHandle = driver.getWindowHandle();
+                voteNow.click();
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Set<String> allWindowHandles = driver.getWindowHandles();
+                Iterator<String> iterator = allWindowHandles.iterator();
+
+                // Here we will check if child window has other child windows and will fetch the heading of the child window
+                while (iterator.hasNext()) {
+                    String ChildWindow = iterator.next();
+                    if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
+                        driver.switchTo().window(ChildWindow);
+                        WebElement nikki = driver.findElement(By.xpath("//img[@alt='Nikki Tamboli']"));
+                        nikki.click();
+                        WebElement submitBtn=driver.findElement(By.xpath("//button[text()='Submit']"));
+                        submitBtn.click();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        wt.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//p[contains(text(),'Thank You for Voting')]"))));
+
+                    }
+                }
+
+            }finally {
+                driver.quit();
             }
         }
-        driver.quit();
+
+
+
     }
 
 
